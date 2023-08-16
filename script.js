79eb0645d8019c815c083df2fefdd74bf2eccbe9
Rhymond <raymond.kazlausk@gmail.com>
2023-08-16 18:48:08 +0300

    opacity 0 for letters

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const letters = document.querySelectorAll(".wrapper > div");
 const input = document.querySelector(".form > input");
+const submit = document.querySelector(".form > .submit");
 const sounds = [];
+
+submit.addEventListener("click", (e) => {
+  alert("You typed: " + input.value);
+  input.value = "";
+});
 
 const random = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -15,37 +21,39 @@ const logslider = (position) => {
   var maxv = Math.log(100);
 
   // calculate adjustment factor
-  var scale = (maxv-minv) / (maxp-minp);
+  var scale = (maxv - minv) / (maxp - minp);
 
-  return Math.exp(minv + scale*(position-minp));
+  return Math.exp(minv + scale * (position - minp));
 };
 
 const shuffleLetters = () => {
-  letters.forEach(l => {
-    l.style.top = random(10, 90) + '%';
-    l.style.left = random(10, 90) + '%';
+  letters.forEach((l) => {
+    l.style.top = random(10, 90) + "%";
+    l.style.left = random(10, 90) + "%";
   });
-}
+};
 shuffleLetters();
 
-letters.forEach(l => {
-  sounds[l.innerHTML] = new Audio('sounds/' + l.innerHTML + '.mp3');
-})
+letters.forEach((l) => {
+  sounds[l.innerHTML] = new Audio("sounds/" + l.innerHTML + ".mp3");
+});
 
 const play = (letter, volume) => {
   sounds[letter].currentTime = 0;
   sounds[letter].volume = volume;
   sounds[letter].play();
-} 
+};
 
 const wrapper = document.querySelector(".wrapper");
-wrapper.addEventListener('click', (e) => {
+wrapper.addEventListener("click", (e) => {
   let distances = [];
-  letters.forEach(l => {
+  letters.forEach((l) => {
     const rect = l.getBoundingClientRect();
     distances.push([
       l.innerHTML,
-      Math.sqrt(Math.pow(e.clientX - rect.x, 2) + Math.pow(e.clientY - rect.y, 2))
+      Math.sqrt(
+        Math.pow(e.clientX - rect.x, 2) + Math.pow(e.clientY - rect.y, 2)
+      ),
     ]);
   });
 
@@ -53,7 +61,7 @@ wrapper.addEventListener('click', (e) => {
     return;
   }
 
-  distances.sort((a, b) => a[1] - b[1])
+  distances.sort((a, b) => a[1] - b[1]);
 
   if (distances[0][1] < 40) {
     play(distances[0][0], 1);
@@ -64,12 +72,12 @@ wrapper.addEventListener('click', (e) => {
   for (let i = 0; i < distances.length; i++) {
     const d = distances[i];
     if (d[1] < max) {
-      play(d[0], logslider(100 - ((d[1] * 100) / max)) / 100)
+      play(d[0], logslider(100 - (d[1] * 100) / max) / 100);
     }
   }
-})
+});
 
-letters.forEach(l => {
+letters.forEach((l) => {
   l.addEventListener("click", (e) => {
     setTimeout(() => {
       input.value = input.value + e.target.innerHTML;
